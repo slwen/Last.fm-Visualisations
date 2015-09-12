078f10fbffb29a8b8d5e5e7b243e7af9d6b5c927
Sam Enoka <slwenoka@gmail.com>
2015-09-12 22:13:17 +1000

    Add getTopArtists route and support for params object.

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _           = require('lodash');
 var getJSONP    = require('./getJSONP');
 var credentials = require("./credentials.json");
 var baseUrl     = "http://ws.audioscrobbler.com/2.0/?format=json&user=slwen&api_key=" + credentials.key;
@@ -8,11 +9,23 @@ var routes = {
   getInfo: baseUrl + "&method=user.getinfo",
   getTopAlbums: baseUrl + "&method=user.gettopalbums",
   getTopTracks: baseUrl + "&method=user.gettoptracks",
+  getTopArtists: baseUrl + "&method=user.gettopartists",
   getRecentTracks: baseUrl + "&method=user.getrecenttracks"
+};
+
+var formatUrl = function(params) {
+  return _.map(params, function(value, key) {
+    return '&' + key + '=' + value;
+  }).join('');
 };
 
 exports.getInfo = function(callback) {
   var route = routes.getInfo;
+  return getJSONP(route, callback);
+};
+
+exports.getTopArtists = function(params, callback) {
+  var route = routes.getTopArtists + formatUrl(params);
   return getJSONP(route, callback);
 };
 
